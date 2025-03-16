99dde4b89f7098f02607a1ae81edb921cbdda1e3
Mykyta Olym <147160141+VetteIT@users.noreply.github.com>
2025-03-17 00:20:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ st.markdown(CUSTOM_CSS, unsafe_allow_html=True)
 # GLOBAL SETTINGS & HYPERPARAMETERS
 ##############################################################################
 # Path to the dataset (Update if necessary)
-DATASET_PATH = r"C:\Users\nikit\PycharmProjects\Bk\DataCollecting\src\results\developer_metrics_final.parquet"
+DATASET_PATH = r"Data\developer_metrics_final.parquet"
 
 # List of numeric and categorical features for processing
 NUMERIC_FEATURES = [
